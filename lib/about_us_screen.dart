1f4e3cbe0e655774a83a14e594bf2cf8d7648c27
6saksham9 <84409831+6saksham9@users.noreply.github.com>
2022-09-15 03:13:53 +0530

    Update about_us_screen.dart

--- lib/about_us_screen.dart
+++ lib/about_us_screen.dart
@@ -22,45 +22,13 @@ class AboutUsScreen extends StatelessWidget {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                boldText("SOFTSOLS PAKISTAN", 20),
+                boldText("Virtual Clinic", 20),
                 Text(
                   "Quality | Technology | Innovation | Customer Satisfaction | Win together",
                   style: TextStyle(color: blue, fontSize: 18),
                   textAlign: TextAlign.center,
                 ),
-                Text(
-                  """
-
-SoftSols Pakistan, as a leading IT solution and service provider, provides innovative information technology - enabled solutions and services to meet the demands arising from social transformation, shaping new life styles for individuals and creating values for the society. 
-Since Inception in 2007, we are offering our clients Strategic IT Consultancy Services for:
->	Corporate IT Solutions 
->	Custom Web Design & Development Services 
->	Digital Marketing Services including Content Marketing, Social Media Marketing, 
->	Email Marketing, Newsletter, Corporate and Brand Videos 
->	Native Mobile Apps Development (Android and IOS) 
->	Gamification Services (Unity 3D Games & 2D Games Development) 
->	Webinar & Web Casting Services & VR Videos  
->	Artificial Intelligence & Automation 
->	Team Launch and Product Launches 
->	Corporate Profile and Documentary 
->	Training and Consultation 
->	Event Management (Theme Setup, A/V, SMD, Printing, Transportation, Catering) 
->	Corporate Holiday Parties and Picnics  
->	3D advertising & Holographic display 
->	Team Building Activities 
->	Medical Conferences and Conventions 
->	Printing Services (Brochure, Banners, Badges, Certificates, Shields, Writing Pads, Bags) 
->	Corporate Catering Services (Hi – Tea, Lunch, Gala Dinners)
-          
-VISIT WWW.SOFTSOLS.PK FOR COMPLETE INFORMATION
-          
-           
-Mr. Kashif Fareed
-C.E.O.
-Softsols Pakistan
-ceo@softsols.pk
-0301-2712507
-                  """,
+                Text("Under development.",
                   style: TextStyle(fontSize: 17),
                   textAlign: TextAlign.justify,
                 ),
