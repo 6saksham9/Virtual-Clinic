2e7eb2ef491d6fdc56a4e7d41792170b50a294ec
6saksham9 <84409831+6saksham9@users.noreply.github.com>
2022-09-15 03:21:12 +0530

    first commit

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -8,7 +8,6 @@ Color? grey3 = Colors.grey[300];
 Color grey = Colors.grey;
 Color? darkGreen = Colors.green[700];
 Color green = Colors.green;
-Color yellow=Colors.yellow;
 Color? green4 = Colors.green[400];
 Color red = Colors.red;
 Color? mc = Colors.green;
